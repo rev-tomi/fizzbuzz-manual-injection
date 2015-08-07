5d4b298bac664ab97f4694320c47d04b20d03667
Tamas Rev <extern.trev@porsche.hu>
2015-08-07 10:35:52 +0200

    fixig the rest of the cases

--- src/main/java/com/trev/fizzbuzz/inject/manual/FizzBuzzTextProvider.java
+++ src/main/java/com/trev/fizzbuzz/inject/manual/FizzBuzzTextProvider.java
@@ -5,6 +5,18 @@ public class FizzBuzzTextProvider
 
     public String getFizzBuzzText(final Integer num)
     {
+        if (num % 15 == 0)
+        {
+            return "FizzBuzz";
+        }
+        if (num % 5 == 0)
+        {
+            return "Buzz";
+        }
+        if (num % 3 == 0)
+        {
+            return "Fizz";
+        }
         return num.toString();
     }
 }
