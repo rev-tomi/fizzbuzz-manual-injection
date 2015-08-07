b72bcd2cf4695a29d25e629259f9231775794091
Tamas Rev <extern.trev@porsche.hu>
2015-08-07 15:55:57 +0200

    retrieving the text for each number

--- src/main/java/com/trev/fizzbuzz/inject/manual/FizzBuzzManualInjection.java
+++ src/main/java/com/trev/fizzbuzz/inject/manual/FizzBuzzManualInjection.java
@@ -4,15 +4,19 @@ public class FizzBuzzManualInjection
 {
 
     private final FizzBuzzNumberProvider numberProvider;
+    private final FizzBuzzTextProvider textProvider;
 
-    public FizzBuzzManualInjection(final FizzBuzzNumberProvider numberProvider)
+    public FizzBuzzManualInjection(final FizzBuzzNumberProvider numberProvider, final FizzBuzzTextProvider textProvider)
     {
         this.numberProvider = numberProvider;
+        this.textProvider = textProvider;
     }
 
     public void doFizzBuzz()
     {
-        numberProvider.getFizzBuzzNumbers();
+        numberProvider.getFizzBuzzNumbers().forEach(num -> {
+            final String msg = textProvider.getFizzBuzzText(num);
+        });
     }
 
 }
--- src/test/java/com/trev/fizzbuzz/inject/manual/FizzBuzzTest.java
+++ src/test/java/com/trev/fizzbuzz/inject/manual/FizzBuzzTest.java
@@ -6,7 +6,9 @@ import static org.mockito.Mockito.when;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
+import org.mockito.InOrder;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -19,6 +21,9 @@ public class FizzBuzzTest
     @Mock
     private FizzBuzzNumberProvider numberProvider;
 
+    @Mock
+    private FizzBuzzTextProvider textProvider;
+
     private Stream<Integer> oneTwoThree;
 
     @BeforeMethod
@@ -27,7 +32,7 @@ public class FizzBuzzTest
         oneTwoThree = IntStream.of(1, 2, 3).boxed();
         MockitoAnnotations.initMocks(this);
         when(numberProvider.getFizzBuzzNumbers()).thenReturn(oneTwoThree);
-        sut = new FizzBuzzManualInjection(numberProvider);
+        sut = new FizzBuzzManualInjection(numberProvider, textProvider);
 
         //        oneTwoThree = IntStream.of(1, 2, 3).boxed();
         //        doReturn(oneTwoThree).when(sut).getFizzBuzzNumbers();
@@ -42,6 +47,19 @@ public class FizzBuzzTest
 
         // THEN
         verify(numberProvider).getFizzBuzzNumbers();
+    }
+
+    @Test
+    public void testTextGenerated()
+    {
+        // WHEN
+        sut.doFizzBuzz();
+
+        // THEN
+        final InOrder order = Mockito.inOrder(textProvider);
+        order.verify(textProvider).getFizzBuzzText(1);
+        order.verify(textProvider).getFizzBuzzText(2);
+        order.verify(textProvider).getFizzBuzzText(3);
     }
 
 }
