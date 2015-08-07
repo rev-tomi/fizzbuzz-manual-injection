51ce7373f5aeceab2dd5bc54ff680d4fb6b2dda2
Tamas Rev <extern.trev@porsche.hu>
2015-08-07 10:32:07 +0200

    copyig text tests from other project

---
+++ src/test/java/com/trev/fizzbuzz/inject/manual/TestFizzBuzzText.java
@@ -0,0 +1,41 @@
+package com.trev.fizzbuzz.inject.manual;
+
+import static org.testng.Assert.assertEquals;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class TestFizzBuzzText
+{
+
+    private FizzBuzzTemplate sut;
+
+    @BeforeMethod
+    void setUp()
+    {
+        sut = new FizzBuzzTemplate();
+    }
+
+    @Test(dataProvider = "fizzBuzzText")
+    public void testFizzBuzzText(final Integer num, final String expected)
+    {
+        // WHEN
+        final String text = sut.getFizzBuzzText(num);
+
+        // THEN
+        assertEquals(text, expected);
+    }
+
+    @DataProvider(name = "fizzBuzzText")
+    private Object[][] getFizzBuzzTextTestData()
+    {
+        return new Object[][]{
+            new Object[]{1, "1"},
+            new Object[]{3, "Fizz"},
+            new Object[]{5, "Buzz"},
+            new Object[]{15, "FizzBuzz"},
+        };
+    }
+
+}
